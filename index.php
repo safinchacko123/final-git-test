05d28424e30f6ee674049b5eff40ec9917b4589a
safinchacko123 <chackosafin@gmail.com>
2016-05-19 18:16:52 +0530

    master edited on server
    master edited on server by sfn

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ and open the template in the editor.
         // put your code here
         //safin adding to master
         //safin master to master 2
+        //safin master edited on server
         ?>
     </body>
 </html>
