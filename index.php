42540b35ff29083344781d83cf3e4f3bf58a69cc
safin <chackosafin@gmail.com>
2016-05-19 18:35:49 +0530

    Branch Lynn  first commit

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        //safin created new branch named Techversant
+        //safin created new branch named Lynn
         ?>
     </body>
 </html>
